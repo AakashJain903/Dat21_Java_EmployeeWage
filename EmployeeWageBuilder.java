3d4b6db45f9b4cc1d93a39c5fab28807879e6f3d
AakashJain903 <aakash.jain903@gmail.com>
2020-08-02 14:20:26 +0530

    Added : Employee Present check method

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,19 +1,16 @@
 class EmployeeWageBuilder
 {
-	 int isFullTime=1;
-    double empCheck = Math.floor(Math.random()*10) % 2;
+	int isFullTime=1;
+   double empCheck = Math.floor(Math.random()*10) % 2;
 
 	public void emp_Present()
 	{
-
 		if (empCheck == isFullTime)
 			System.out.println("Employee is Present");
 
 		else
 			System.out.println("Employee is Absent");
-
 	}
-
 
 	public static void main(String arg[])
 	{
