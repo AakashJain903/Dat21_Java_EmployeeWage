3e0bd761d2c5925e5eaafc00f3c008fe2304705f
AakashJain903 <aakash.jain903@gmail.com>
2020-08-02 17:53:41 +0530

    Added : Refactor code

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -10,8 +10,17 @@ class EmployeeWageBuilder
 	int totalWage=0;
 	int totalWorkingDay=0;
 	int totalWorkingHrs=0;
+  	
+   public void wageComputation()
+   {
+   		empWage = empHrs * empRatePerHrs ;
+			totalWage += empWage;
+			totalWorkingHrs += empHrs;
+			System.out.println("Employee's day "+ totalWorkingDay +" Wage is : " + empWage);
+	}
+
   	public void empWage()
-	{
+  	{
 		while ( totalWorkingDay < numOfWorkingDays && totalWorkingHrs < totalHrsInMonth )
 		{
 			totalWorkingDay++;
@@ -32,15 +41,13 @@ class EmployeeWageBuilder
 					System.out.println("Employee is Absent");
 					empHrs = 0;
 			}
-
-			empWage = empHrs * empRatePerHrs ;
-			totalWage += empWage;
-			totalWorkingHrs += empHrs;
-			System.out.println("Employee's day "+ totalWorkingDay +" Wage is : " + empWage);
+			wageComputation();
 		}
+		
 		System.out.println("Total working Hours in month : " + totalWorkingHrs);
    	System.out.println("Total Monthly wages is :" + totalWage);
    }
+
 
    public static void main(String arg[])
    {
