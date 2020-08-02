22d7b282d04cd27131e45373441ce9681c351bdb
AakashJain903 <aakash.jain903@gmail.com>
2020-08-02 17:58:50 +0530

    Added : Refactor code

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,4 +1,5 @@
-class EmployeeWageBuilder{
+class EmployeeWageBuilder
+{
 	static final int isFullTime=1;
 	static final int isPartTime=2;
 	static final int empRatePerHrs=20;
@@ -10,18 +11,22 @@ class EmployeeWageBuilder{
 	int totalWorkingDay=0;
 	int totalWorkingHrs=0;
   	
-   public void wageComputation(){
+   public void wageComputation()
+   {
    	empWage = empHrs * empRatePerHrs ;
 		totalWage += empWage;
 		totalWorkingHrs += empHrs;
 		System.out.println("Employee's day "+ totalWorkingDay +" Wage is : " + empWage);
 	}
 
-  	public void empWage(){
-		while ( totalWorkingDay < numOfWorkingDays && totalWorkingHrs < totalHrsInMonth ){
+  	public void empWage()
+  	{
+		while ( totalWorkingDay < numOfWorkingDays && totalWorkingHrs < totalHrsInMonth )
+		{
 			totalWorkingDay++;
 			int empCheck = (int) Math.floor(Math.random()*10) % 3;
-			switch (empCheck){
+			switch (empCheck)
+			{
 				case isFullTime :
 					System.out.println("Employee is full time working");
 					empHrs = 8;
@@ -43,7 +48,8 @@ class EmployeeWageBuilder{
    	System.out.println("Total Monthly wages is :" + totalWage);
    }
 
-   public static void main(String arg[]){
+   public static void main(String arg[])
+   {
       EmployeeWageBuilder wage = new EmployeeWageBuilder();
       wage.empWage();
    }
