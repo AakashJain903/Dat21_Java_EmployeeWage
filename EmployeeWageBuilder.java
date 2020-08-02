de6e2a231d1a1a3069032f811f598bf41d8ced86
AakashJain903 <aakash.jain903@gmail.com>
2020-08-02 17:57:06 +0530

    Added : Refactor code

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,5 +1,4 @@
-class EmployeeWageBuilder
-{
+class EmployeeWageBuilder{
 	static final int isFullTime=1;
 	static final int isPartTime=2;
 	static final int empRatePerHrs=20;
@@ -11,22 +10,18 @@ class EmployeeWageBuilder
 	int totalWorkingDay=0;
 	int totalWorkingHrs=0;
   	
-   public void wageComputation()
-   {
-   		empWage = empHrs * empRatePerHrs ;
-			totalWage += empWage;
-			totalWorkingHrs += empHrs;
-			System.out.println("Employee's day "+ totalWorkingDay +" Wage is : " + empWage);
+   public void wageComputation(){
+   	empWage = empHrs * empRatePerHrs ;
+		totalWage += empWage;
+		totalWorkingHrs += empHrs;
+		System.out.println("Employee's day "+ totalWorkingDay +" Wage is : " + empWage);
 	}
 
-  	public void empWage()
-  	{
-		while ( totalWorkingDay < numOfWorkingDays && totalWorkingHrs < totalHrsInMonth )
-		{
+  	public void empWage(){
+		while ( totalWorkingDay < numOfWorkingDays && totalWorkingHrs < totalHrsInMonth ){
 			totalWorkingDay++;
 			int empCheck = (int) Math.floor(Math.random()*10) % 3;
-			switch (empCheck)
-			{
+			switch (empCheck){
 				case isFullTime :
 					System.out.println("Employee is full time working");
 					empHrs = 8;
@@ -48,9 +43,7 @@ class EmployeeWageBuilder
    	System.out.println("Total Monthly wages is :" + totalWage);
    }
 
-
-   public static void main(String arg[])
-   {
+   public static void main(String arg[]){
       EmployeeWageBuilder wage = new EmployeeWageBuilder();
       wage.empWage();
    }
