6826d818f86e36251179d85251a0989f513cc59b
AakashJain903 <aakash.jain903@gmail.com>
2020-08-02 14:58:17 +0530

    Added : Employee daily wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,20 +1,31 @@
 class EmployeeWageBuilder
 {
-	int isFullTime=1;
-   double empCheck = Math.floor(Math.random()*10) % 2;
+	static final int isFullTime=1;
+	static final int empRatePerHrs=20;
+	int empHrs=0;
+	int empWage=0;
+	
+   	double empCheck = Math.floor(Math.random()*10) % 2;
 
-	public void emp_Present()
+	public void empDailyWage()
 	{
 		if (empCheck == isFullTime)
+		{
 			System.out.println("Employee is Present");
-
+			empHrs=8;
+		}
 		else
+		{
 			System.out.println("Employee is Absent");
-	}
+			empHrs=0;
+		}
+		empWage = empHrs*empRatePerHrs;
+		System.out.println("Employee wage : " + empWage);
 
+	}
 	public static void main(String arg[])
 	{
-		EmployeeWageBuilder presentChk = new EmployeeWageBuilder();
-		presentChk.emp_Present();
+		EmployeeWageBuilder wage = new EmployeeWageBuilder();
+		wage.empDailyWage();
 	}
 }
